519731601eef4b4b6db96d543d414111f7182e07
남주영 <skawndud321@gmail.com>
2018-11-30 21:02:58 +0900

    Revert "h"
    This reverts commit 5b7c50958b8bf1d90c73f3f1532b6622494f6927.

--- Singame.cpp
+++ Singame.cpp
@@ -202,7 +202,7 @@ GLuint LoadTexture(const char * filename, int width, int height)
 	unsigned char * data;
 	FILE* file;
 
-	fopen_s(&file, filename, "rb");
+	file = fopen(filename, "rb");
 	if (file == NULL) return 0;
 	data = (unsigned char *)malloc(width * height * 3 + 54);
 	fread(data, width * height * 3 + 54, 1, file);
